6b39da564eb67e56f58164a321e29f85fc3d4464
VEERAGRANDHI <v.v.grandhi@wlv.ac.uk>
2024-06-27 07:54:55 +0530

    Only vehicles

--- routes/api.php
+++ routes/api.php
@@ -24,11 +24,3 @@ Route::get('/vehicles/{id}', [VehiclesController::class, 'show']);
 Route::post('/vehicles', [VehiclesController::class, 'store']);
 Route::put('/vehicles/{id}', [VehiclesController::class, 'update']);
 Route::delete('/vehicles/{id}', [VehiclesController::class, 'destroy']);
-
-
-// Implement service appointment routes
-Route::get('/appointments', [ServiceAppointmentsController::class, 'index']);
-Route::get('/appointments/{id}', [ServiceAppointmentsController::class, 'show']);
-Route::post('/appointments', [ServiceAppointmentsController::class, 'store']);
-Route::put('/appointments/{id}', [ServiceAppointmentsController::class, 'update']);
-Route::delete('/appointments/{id}', [ServiceAppointmentsController::class, 'destroy']);
